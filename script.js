a26aa45998dfbe4256dd5b7cce52afe80f1d7089
justinmm777 <justinmm777@gmail.com>
2021-11-04 09:53:59 +0000

    Added basic math functions and operate function

---
+++ script.js
@@ -0,0 +1,37 @@
+
+// Operater function
+function operate(operator, a, b) {
+    if (operator == "+") {
+        return add(a, b);
+    } else if (operator == "-") {
+        return subtract(a, b);
+    } else if (operator == "*") {
+        return multiply(a, b);
+    } else if (operator == "/") {
+        return divide(a, b);
+    }
+}
+
+
+// functions for all of the basic math operators
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        return "Cannot divide by Zero!"
+    }
+    return a / b;
+}
+
+
+console.log(operate("/", 8, 4));
