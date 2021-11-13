710a75ac3d2fa8fef58241904d26c319c04b615a
justinmm777 <justinmm777@gmail.com>
2021-11-13 20:58:45 +0000

    Rewriting html and css

--- script.js
+++ script.js
@@ -1,60 +1,14 @@
 
-const operatorDisplay = document.getElementById('operatorDisplay');
-const historyDisplay = document.getElementById('historyDisplay');
-const liveDisplay = document.getElementById('liveDisplay');
-const numbers = document.querySelectorAll('.btnNum');
-const equal = document.getElementById('btnEqual');
-const operators = document.querySelectorAll('.btnOperator');
 
 // store the first number that is input 
 // into the calculator when a user presses an operator
-operators.forEach(item => {
-    item.addEventListener('click', event => {
-        operatorDisplay.textContent = item.value;
-        historyDisplay.textContent = liveDisplay.textContent;
-        clearLiveDisplay();
-    })
-})
 
-equal.addEventListener('click', event => {
-    if (operatorDisplay.textContent === "=") {
-        return;
-    }
-    const operator = operatorDisplay.textContent;
-    const num1 = parseInt(historyDisplay.textContent);
-    const num2 = parseInt(liveDisplay.textContent);
-    const result = operate(operator, num1, num2)
-    console.log(result);
-    clearHistoryDisplay();
-    clearOperatordisplay();
-    operatorDisplay.textContent = equal.value;
-    historyDisplay.textContent = `${num1} ${operator} ${num2}`;
-    liveDisplay.textContent = `${result}`;
-});
+
 
 // save which operation has been chosen
 // then operate() on them when the user presses the “=” key. 
 
 // function to clear the content of liveDisplay
-function clearLiveDisplay() {
-    liveDisplay.textContent = "";
-}
-
-function clearHistoryDisplay() {
-    historyDisplay.textContent = "";
-}
-
-function clearOperatordisplay() {
-    operatorDisplay.textContent = "";
-}
-
-// // function to populate display
-numbers.forEach(item => {
-    item.addEventListener('click', event => {
-        liveDisplay.textContent += item.value;
-    });
-});
-
 
 
 // Operater function
